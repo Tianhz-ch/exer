08a3ff1ce7f68beb65d626f9a841e3689cd13429
Tianhz-ch <tianhzch@gmail.com>
2020-09-08 01:14:59 +0800

    leetcode 141

---
+++ src/main/leetcode/LC141.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+import base.ListNode;
+
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where the tail connects to.
+ * If pos == -1, then there is no cycle in the linked list.
+ *
+ * @author neilly
+ */
+public class LC141 {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
