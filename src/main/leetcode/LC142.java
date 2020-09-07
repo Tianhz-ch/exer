d51a060236d73ae759a4f0138f621fa6dccd46ea
Tianhz-ch <tianhzch@gmail.com>
2020-09-08 01:25:43 +0800

    leetcode 142

---
+++ src/main/leetcode/LC142.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import base.ListNode;
+
+/**
+ * Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to.
+ * If pos is -1, then there is no cycle in the linked list.
+ *
+ * @author neilly
+ */
+public class LC142 {
+
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode slow = head, fast = head;
+        while (slow != null && fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
